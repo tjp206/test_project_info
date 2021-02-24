01705baa001aa3b0d5c3005db47aa0db8fc171d6
TJ <tjpataria@TJs-MacBook-Pro.local>
2021-02-24 12:09:30 +0000

    added hello-world function Co-authored-by: David Strain <strd79@hotmail.co.uk>

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 # Test
 
-def hello_tj
-    print "hello TJ"
+def hello_tj():
+    print ("hello TJ")
 
 
-def hello_david
-    print "hello David"
+def hello_david():
+    print ("hello David")
+
+def hello_world():
+    print ("hello world")
