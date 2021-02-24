4320d25ba326d5ef7fa12e2871e229c7c108cef8
David Strain <strd79@hotmail.co.uk>
2021-02-24 14:28:25 +0000

    called the functions

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
 # Test
 
 def hello_tj():
-    print ("hello TJ")
-
+    print("hello TJ")
 
 def hello_david():
-    print ("hello David")
+    print("hello David")
 
 def hello_world():
-    print ("hello world")
+    print("hello world")
 
-def what_time_is_it();
-    print ("The time is: !")    
+def what_time_is_it():
+    print("The time is: !")    
 
 def hello_koala():
-    print ("Hello Koala!")
+    print("Hello Koala!")
 
 
-
+hello_tj()
+hello_david()
+hello_world()
+what_time_is_it()
+hello_koala()
