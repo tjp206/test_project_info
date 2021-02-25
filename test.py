261ca3ee5e6020b4640d975c9b87cba2b26f0c59
David Strain <strd79@hotmail.co.uk>
2021-02-25 15:34:14 +0000

    created horse function
    Co-authored-by: TJ Pataria <tjpataria@gmail.com>

--- test.py
+++ test.py
@@ -27,6 +27,9 @@ def hello_linsey():
 def hello_craig():
     print("Hello Craig!")
 
+def speak_to_mr_horse():
+    print("Hello Mr Horse!")
+
 hello_tj()
 hello_david()
 hello_world()
@@ -36,3 +39,4 @@ hello_dog()
 hello_lion()
 hello_linsey()
 hello_craig()
+speak_to_mr_horse()
